869d31cffc255ffd6715fc3484d4480c63b065ec
DedSeK404 <marwenbennaceur5@gmail.com>
2026-08-20 17:48:39 +0100

    sign in page creation

--- Client/src/Components/Login/SignIn/SignIn.tsx
+++ Client/src/Components/Login/SignIn/SignIn.tsx
@@ -1,17 +1,36 @@
-import React from "react";
-
 export default function SignIn() {
   return (
-    <div>
-      <div className="aura aura-[#aa3bff] rounded-lg">
-        <button type="button" className="btn btn-primary px-6 py-2.5">
-          Sign In
-        </button>
-      </div>
+    <div className="min-h-screen w-full flex items-center justify-center bg-linear-to-bl from-[#9073E9] to-[#5A39C6]">
+      <div className="card card-side bg-base-100 shadow-sm">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            alt="Movie"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-2xl">
+            <span className="font-bold text-4xl text-[#5B3AC9]">Login</span>{" "}
+            your account
+          </h2>
+          <form className="space-y-4">
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Username</legend>
+              <input type="email" className="input" placeholder="Type your email here" />
+            </fieldset>
 
-      <button type="button" className="btn btn-primary px-6 py-2.5">
-        Create Account
-      </button>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input type="password" className="input" placeholder="*********" />
+            </fieldset>
+            <div className="aura">
+              <button className="btn btn-wide btn-soft btn-primary">
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
