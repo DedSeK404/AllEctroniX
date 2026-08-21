2b8b9f3518cf090fd98e992b8a1f9b955677c52b
DedSeK404 <marwenbennaceur5@gmail.com>
2026-08-21 15:33:11 +0100

    signUp Page

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,14 +1,19 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from "./Components/DashBoard/DashBoard"
+import SignUp from "./Components/Login/SignUp/SignUp"
 import SignIn from './Components/Login/SignIn/SignIn';
 
 function App() {
   return (
     <div>
-      <Routes>     
-      <Route path="/login" element={<SignIn />} />   
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
+      <Routes>    
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login/signin" element={<SignIn />} /> 
+        <Route path="/login/signup" element={<SignUp />} />
+        {/* Redirect root "/" and fallback "*" straight to "/dashboard" */}
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
     </div>
   );
 }
--- Client/src/Components/Login/SignUp/SignUp.tsx
+++ Client/src/Components/Login/SignUp/SignUp.tsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import { Eye, EyeOff, Mail } from "lucide-react";
 import loginArt from "../../../assets/images/loginArt.svg";
+import { useState } from "react";
+import { Eye, EyeOff, Mail, User } from "lucide-react";
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
-
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const navigate = useNavigate();
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-linear-to-bl from-[#9073E9] to-[#5A39C6] p-4 sm:p-6 lg:p-8">
@@ -24,14 +24,33 @@ export default function SignIn() {
         <div className="card-body w-full md:w-1/2 p-6 sm:p-12 overflow-y-auto flex flex-col justify-around">
           <h2 className="card-title text-2xl flex flex-row items-baseline justify-center gap-2">
             <span className="font-bold text-3xl sm:text-5xl text-[#B100D6]">
-              Login
+              Create
             </span>
             <span className="text-xl sm:text-3xl text-neutral-300 font-normal">
-              your account
+              account
             </span>
           </h2>
 
           <form className="space-y-4">
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend text-neutral-300 text-lg">
+                Full name
+              </legend>
+              <div className="relative w-full">
+                <input
+                  required
+                  type="email"
+                  className="input input-bordered w-full bg-neutral-900 border-neutral-700 text-white placeholder:text-neutral-500"
+                  placeholder="Type your username here"
+                />
+                <button
+                  type="button"
+                  className="absolute right-3 top-1/2 -translate-y-1/2 text-neutral-400  "
+                >
+                  <User className="w-5 h-5" />
+                </button>
+              </div>
+            </fieldset>
             <fieldset className="fieldset">
               <legend className="fieldset-legend text-neutral-300 text-lg">
                 Email
@@ -76,16 +95,40 @@ export default function SignIn() {
                 </button>
               </div>
             </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend text-neutral-300 text-lg">
+                Confirm password
+              </legend>
+              <div className="relative w-full">
+                <input
+                  required
+                  type={showConfirmPassword ? "text" : "password"}
+                  className="input input-bordered w-full bg-neutral-900 border-neutral-700 text-white placeholder:text-neutral-500 pr-10"
+                  placeholder="*********"
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 text-neutral-400 hover:text-white transition-colors cursor-pointer"
+                >
+                  {showConfirmPassword ? (
+                    <EyeOff className="w-5 h-5" />
+                  ) : (
+                    <Eye className="w-5 h-5" />
+                  )}
+                </button>
+              </div>
+            </fieldset>
 
             <div className="pt-4">
               <button className="btn btn-primary w-full text-lg">
-                Sign in
+                Sign Up
               </button>
               <p className="mt-7 text-xl">
-                Don't have an account?{" "}
+                Already have an account?{" "}
                 <a
                   className="link link-hover text-[#B100D6]"
-                  onClick={() => navigate("/login/signup")}
+                  onClick={() => navigate("/login/signin")}
                 >
                   Click here
                 </a>
